ac8b87ded7898cbc4671072ae46882d7c36e87ba
nka2001 <nandrle01@gmail.com>
2021-12-01 12:21:50 -0500

    lab

---
+++ CSCweek14_Lab/src/main/java/com/mycompany/cscweek14_lab/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cscweek14_lab;
+
+/**
+ *
+ * @author nicka
+ */
+
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    
+    
+    public static void main(String[]args){
+        Stack s = new Stack();
+        String line; // the string of characters to be checked
+        Scanner scan = new Scanner(System.in);
+        System.out.println ("\nParenthesis Matching");
+        System.out.print ("Enter a parenthesized expression: ");
+        line = scan.nextLine();
+        
+        for(int i = 0; i < line.length(); i++){
+            
+        }
+
+    }
+    
+}
