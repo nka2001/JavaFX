2a2b98f0d26906a96790afda7306b90c7ce922b7
nka2001 <nandrle01@gmail.com>
2021-12-03 11:35:44 -0500

    Problem 2

--- CSCweek14_Lab/src/main/java/com/mycompany/cscweek14_lab/Problem02.java
+++ CSCweek14_Lab/src/main/java/com/mycompany/cscweek14_lab/Problem02.java
@@ -5,12 +5,40 @@
  */
 package com.mycompany.cscweek14_lab;
 
+import java.util.*;
+
 /**
  *
  * @author nicka
  */
 public class Problem02 {
-    
+    public static void main(String[]args){
+        Stack<String> ops = new Stack<String>();
+        Stack<Double> vals = new Stack<Double>();
+        Scanner stdin= new Scanner("( 1 + 5 ) ");
+        while(stdin.hasNext()){
+            String s = stdin.next();
+            if(s.equals("(")){}
+            else if(s.equals("+"))  ops.push(s);
+            else if(s.equals("-"))  ops.push(s);
+            else if(s.equals("*"))  ops.push(s);
+            else if(s.equals("/"))  ops.push(s);
+            else if(s.equals(")")){
+                String op = ops.pop();
+                double v = vals.pop();
+                if(op.equals("+"))  v = vals.pop() + v;
+                if(op.equals("-"))  v = vals.pop() - v;
+                if(op.equals("*"))  v = vals.pop() * v;
+                if(op.equals("/"))  v = vals.pop() / v;
+                vals.push(v);
+                    }
+                else{
+                    vals.push(Double.parseDouble(s));
+                    }
+            System.out.println(vals.pop());
+        }
+        
+    }
     
     
 }
