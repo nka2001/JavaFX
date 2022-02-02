63b16d76f9f0063db2ab9ad24ab2d2c04b529734
nka2001 <nandrle01@gmail.com>
2022-02-02 14:37:50 -0500

    Part of GUI is started, next comes the code for getting an order

---
+++ JavaFXProject1/src/main/java/com/mycompany/javafxproject1/PrimaryController.java
@@ -0,0 +1,44 @@
+package com.mycompany.javafxproject1;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+public class PrimaryController {
+
+    @FXML
+    private ProgressBar orderProgress;
+    @FXML
+    private Button CompleteButton;
+    @FXML
+    private RadioButton Pickup;
+    @FXML
+    private ToggleGroup pord;
+    @FXML
+    private RadioButton deliver;
+    @FXML
+    private ToggleGroup EntreeSelect;
+    @FXML
+    private RadioButton Steak;
+    @FXML
+    private RadioButton Chicken;
+    @FXML
+    private RadioButton Lamb;
+    @FXML
+    private RadioButton Salmon;
+
+    @FXML
+    private void CompleteOrder(ActionEvent event) {
+        
+       if()
+        
+       
+        
+    }
+
+   
+}
